fd213837ae2323f3a4e66cbab11bcee57c26d156
Yash Jangid <yash.jangid000@gmail.com>
2024-03-28 18:18:12 +0530

    fix: get coordinates on element click

--- src/Component Board/Chessboard/ChessBoard.jsx
+++ src/Component Board/Chessboard/ChessBoard.jsx
@@ -5,8 +5,6 @@ const verticalaxis = ['1', '2', '3', '4', '5', '6', '7', '8'];
 const horizontalaxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 const piece = [];
 
-
-
 for (let i = 0; i < 8; i++) {
   piece.push({ image: 'Chess_pdt60.png', x: i, y: 6 });
 }
@@ -14,10 +12,10 @@ for (let i = 0; i < 8; i++) {
 for (let i = 0; i < 8; i++) {
   piece.push({ image: 'Chess_plt60.png', x: i, y: 1 });
 }
-for(let p=0;p<2;p++){
-  const type= p==0?"d":"l";
-  const y= p==0?7:0;
-   piece.push({ image: `Chess_r${type}t60.png`, x: 0, y: y });
+for (let p = 0; p < 2; p++) {
+  const type = p === 0 ? 'd' : 'l';
+  const y = p === 0 ? 7 : 0;
+  piece.push({ image: `Chess_r${type}t60.png`, x: 0, y: y });
   piece.push({ image: `Chess_r${type}t60.png`, x: 7, y: y });
   piece.push({ image: `Chess_n${type}t60.png`, x: 1, y: y });
   piece.push({ image: `Chess_b${type}t60.png`, x: 2, y: y });
@@ -28,23 +26,36 @@ for(let p=0;p<2;p++){
 }
 
 function ChessBoard() {
+  const clickConsole = (x, y) => {
+    console.log('Clicked at coordinates:', { x, y });
+  };
+
   let board = [];
 
   for (let j = verticalaxis.length - 1; j >= 0; j--) {
     for (let i = 0; i < horizontalaxis.length; i++) {
       let number = j + i + 2;
       let image = undefined;
-      let x="";
-      let y="";
+      let x;
+      let y;
       piece.forEach(p => {
         if (p.x === i && p.y === j) {
           image = p.image;
-          x=i;
-          y=j;
-          console.log(x,y);
+          x = i;
+          y = j;
         }
       });
-      board.push(<Tile key={`${i}${j}`} click={clickConsole} x={x} y={x}  number={number} image={image} />);
+
+      board.push(
+        <Tile
+          key={`${i}${j}`}
+          click={clickConsole}
+          x={x}
+          y={y}
+          number={number}
+          image={image}
+        />
+      );
     }
   }
   return <div className='chessboard'>{board}</div>;
--- src/Component Board/Tile/Tile.jsx
+++ src/Component Board/Tile/Tile.jsx
@@ -1,27 +1,18 @@
 import './Tile.css';
 
-function Tile({ number, image ,clickConsole}) 
-
-const clickConsole = (x, y) => {
-  console.log("Clicked at coordinates:", x, y);
-}
-
-{
-  if (number % 2 == 0) {
+function Tile({ number, image, click, x, y }) {
+  if (number % 2 === 0) {
     return (
-      <div onClick={clickConsole} className='tile black-tile'>
+      <div onClick={() => click(x, y)} className='tile black-tile'>
         <img id='chessImg' src={image} />
       </div>
     );
-  } 
-  else {
+  } else {
     return (
-      <div  className='tile white-tile'>
+      <div className='tile white-tile' onClick={() => click(x, y)}>
         <img id='chessImg' src={image} />
       </div>
     );
-
-
   }
 }
 export default Tile;
