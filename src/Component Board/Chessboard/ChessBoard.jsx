ede4d19740de75303bcf10f4421ebc0b6550de88
vikasgaur56 <vikasgaur840@gmail.com>
2024-03-31 10:56:24 +0530

    formatting changes

--- src/Component Board/Chessboard/ChessBoard.jsx
+++ src/Component Board/Chessboard/ChessBoard.jsx
@@ -1,22 +1,20 @@
-import './ChessBoard.css';
-import Tile from '../Tile/Tile.jsx';
-import {useState} from "react";
+import "./ChessBoard.css";
+import Tile from "../Tile/Tile.jsx";
+import { useState } from "react";
 
-const verticalaxis = ['1', '2', '3', '4', '5', '6', '7', '8'];
-const horizontalaxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+const verticalaxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+const horizontalaxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 const piece = [];
 
-
-
 for (let i = 0; i < 8; i++) {
-  piece.push({ image: 'Chess_pdt60.png', x: i, y: 6 });
+  piece.push({ image: "Chess_pdt60.png", x: i, y: 6 });
 }
 
 for (let i = 0; i < 8; i++) {
-  piece.push({ image: 'Chess_plt60.png', x: i, y: 1 });
+  piece.push({ image: "Chess_plt60.png", x: i, y: 1 });
 }
 for (let p = 0; p < 2; p++) {
-  const type = p === 0 ? 'd' : 'l';
+  const type = p === 0 ? "d" : "l";
   const y = p === 0 ? 7 : 0;
   piece.push({ image: `Chess_r${type}t60.png`, x: 0, y: y });
   piece.push({ image: `Chess_r${type}t60.png`, x: 7, y: y });
@@ -29,37 +27,45 @@ for (let p = 0; p < 2; p++) {
 }
 
 function ChessBoard() {
-
-const [color,setColor]= useState(false);
+  const [color, setColor] = useState(false);
 
   const clickConsole = (x, y) => {
-    console.log('Clicked at coordinates:', { x, y });
-    if ( (x === 0 && y===7) || (x===7 && y===7) || (x===0 && y===0) || (x===7 && y===0)){
+    console.log("Clicked at coordinates:", { x, y });
+    if (
+      (x === 0 && y === 7) ||
+      (x === 7 && y === 7) ||
+      (x === 0 && y === 0) ||
+      (x === 7 && y === 0)
+    ) {
       console.log("elephant");
-    }
-    else if ( (x===2 && y===7) || (x===5 && y===7) || (x===2 && y===0) || (x===5 && y===0)){
-console.log("camel")
-    }
-    else if( (x===1 && y===7) || (x===6 && y===7) || (x===1 && y===0) || (x===6 && y===0) ){
-console.log("horse");
-    }
-    else if((x===3 && y===7) || (x===3 && y===0)){
+    } else if (
+      (x === 2 && y === 7) ||
+      (x === 5 && y === 7) ||
+      (x === 2 && y === 0) ||
+      (x === 5 && y === 0)
+    ) {
+      console.log("camel");
+    } else if (
+      (x === 1 && y === 7) ||
+      (x === 6 && y === 7) ||
+      (x === 1 && y === 0) ||
+      (x === 6 && y === 0)
+    ) {
+      console.log("horse");
+    } else if ((x === 3 && y === 7) || (x === 3 && y === 0)) {
       console.log("queen");
+    } else if ((x === 4 && y === 7) || (x === 4 && y === 0)) {
+      console.log("king");
     }
-else if((x===4 && y===7) || (x===4 && y===0)){
-console.log("king");
-}
-for(let i=0;i<8;i++){
-  if(x===i && y===6){
-  console.log("soldier",x,y+1);
-  setColor(true);
-}
-else if(x===i && y===1){
-console.log("soldier",x,y+1);
-setColor(true);
-
-}
-}
+    for (let i = 0; i < 8; i++) {
+      if (x === i && y === 6) {
+        console.log("soldier", x, y + 1);
+        setColor(true);
+      } else if (x === i && y === 1) {
+        console.log("soldier", x, y + 1);
+        setColor(true);
+      }
+    }
   };
 
   let board = [];
@@ -68,19 +74,17 @@ setColor(true);
     for (let i = 0; i < horizontalaxis.length; i++) {
       let number = j + i + 2;
       let image = undefined;
-      let x=i;
-      let y=j;
-      piece.forEach(p => {
+      let x = i;
+      let y = j;
+      piece.forEach((p) => {
         if (p.x === i && p.y === j) {
           image = p.image;
-          // x = p.x;
-          // y = p.y;
         }
       });
 
       board.push(
         <Tile
-        className={`${color}?colorChange:''`}
+          className={`${color}?colorChange:''`}
           key={`${i}${j}`}
           click={clickConsole}
           x={x}
@@ -91,7 +95,6 @@ setColor(true);
       );
     }
   }
-  return <div className='chessboard'>{board}</div>;
+  return <div className="chessboard">{board}</div>;
 }
 export default ChessBoard;
-
